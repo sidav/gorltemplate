fc802dd495af8706005448a0af5a462e4ed90d9e
vkovun <sidav94@gmail.com>
2021-06-28 16:10:57 +0300

    WIP.

--- level.go
+++ level.go
@@ -5,6 +5,34 @@ type level struct {
 	actors []*actor
 }
 
+func (l *level) isAnyActorPresentAt(x, y int) bool {
+	for i := range l.actors {
+		if l.actors[i].x == x && l.actors[i].y == y {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *level) getFirstActorAtCoords(x, y int) *actor {
+	for i := range l.actors {
+		if l.actors[i].x == x && l.actors[i].y == y {
+			return l.actors[i]
+		}
+	}
+	return nil
+}
+
+func (l *level) getAllActorsAtCoords(x, y int) []*actor {
+	var actors []*actor
+	for i := range l.actors {
+		if l.actors[i].x == x && l.actors[i].y == y {
+			actors = append(actors, l.actors[i])
+		}
+	}
+	return actors
+}
+
 func (l *level) moveActorByVector(a *actor, vx, vy int) {
 	x, y := a.x, a.y
 	if l.coordsValid(x+vx, y+vy) && l.tiles[x+vx][y+vy].isPassable() {
